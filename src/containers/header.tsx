4bae18f551e7215ec4a119501be240b7fc402324
nikolayz <nikolay.zhekov@onseostudio.com>
2024-07-08 14:22:48 +0300

    feat(containers): implement inital containers ref(old-styles): cleanup old styles

---
+++ src/containers/header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div></div>
+        <a href="/">
+          <span>Nikolay Zhekov</span>
+          <span>/ SOFTWARE ENGINEER</span>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
