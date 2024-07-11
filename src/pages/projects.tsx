9e22b2338cecbeeadaec993a7dcd1d683d55c907
jekovniki <jekovniki95@gmail.com>
2024-07-11 20:22:48 +0300

    styles(projects-page): make projects page responsive

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -12,10 +12,8 @@ const Projects: FC = () => {
       <div className="content-box">
         <Title>Projects</Title>
         <div className={`description ${loaded ? "loaded" : ""}`}>
-          I'm a paragraph. Click here to add your own text and edit me. It’s
-          easy. Just click “Edit Text” or double click me to add your own
-          content and make changes to the font. I’m a great place for you to
-          tell a story and let your users know a little more about you.
+          Here are some of the projects that I have worked on (part-time) during
+          my time as a developer.
         </div>
         {PROJECTS.map((project) => (
           <ProjectBox
