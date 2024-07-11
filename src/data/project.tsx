c63e2c751efd170cac367da97f4c310bcc4591fa
jekovniki <jekovniki95@gmail.com>
2024-07-11 20:12:15 +0300

    feat(data): update projects text

--- src/data/project.tsx
+++ src/data/project.tsx
@@ -7,12 +7,43 @@ export const PROJECTS: ProjectBoxProps[] = [
     name: "Commenter.ai",
     role: "Full-stack developer and architect",
     image: commenterImage,
-    text: "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.",
+    text: (
+      <>
+        <p>
+          By customer request I build a full-stack app that implements the
+          ChatGPT API to answer for comments on LinkedIn and etc.
+        </p>
+        <p>
+          I used ReactJS for Front-end, NodeJS for backend and the database that
+          I used is Firestore. It's hosted on AWS server.
+        </p>
+      </>
+    ),
   },
   {
     name: "Camera widget",
     role: "Main developer",
     image: drupalImage,
-    text: "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.",
+    text: (
+      <>
+        <p>
+          By customer request I build a custom plugin for Drupal 9, using PHP
+          and Javascript.
+        </p>
+        <p>
+          The plugin implements the WebRTC capturing in order to capture an
+          image from the admin's phone and directly upload it to the website.
+        </p>
+        <p>
+          <a
+            href="https://github.com/jekovniki/camera_widget"
+            target="_blank"
+            className="colored-link"
+          >
+            See project
+          </a>
+        </p>
+      </>
+    ),
   },
 ];
